cb7f07696e43ac7e2c117c306959741691b5e2be
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-10-02 20:34:52 +0100

    Enabling the building of all images

--- jx-docker-build.sh
+++ jx-docker-build.sh
@@ -27,6 +27,7 @@ docker version
 echo "====================================="
 
 docker build -t $ORG/jenkins-filerunner:$TAG -f Dockerfile-filerunner .
+head -n 1 Dockerfile-base
 echo "Built $ORG/jenkins-filerunner:$TAG"
 
 sed -i -e "s/FROM .*/FROM ${ORG}\/jenkins-filerunner:${TAG}/" Dockerfile-base
@@ -34,19 +35,19 @@ head -n 1 Dockerfile-base
 docker build -t $ORG/jenkins-base:$TAG -f Dockerfile-base .
 echo "Built $ORG/jenkins-base:$TAG"
 
-exit 1
-
 declare -a arr=("maven" "javascript" "go" "gradle" "python" "scala" "rust" "csharp" "jenkins" "cwp")
 
 ## now loop through the above array
 for i in "${arr[@]}"
 do
     echo "building builder-$i"
-    docker build --build-arg ORG=$ORG --build-arg BASE_TAG=$TAG -t $ORG/jenkins-$i:$TAG -f Dockerfile-$i .
+	sed -i -e "s/FROM .*/FROM ${ORG}\/jenkins-base:${TAG}/" Dockerfile-$i
+	head -n 1 Dockerfile-$i
+    docker build -t $ORG/jenkins-$i:$TAG -f Dockerfile-$i .
 done
 
-#for i in "${arr[@]}"
-#do
-#    echo "pushing builder-$i"
+for i in "${arr[@]}"
+do
+    echo "pushing builder-$i"
 #    docker push $ORG/jenkins-$i:$TAG
-#done
+done
