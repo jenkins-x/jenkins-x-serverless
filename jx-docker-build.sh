5a28633076e131efa3f9e51e27aab54b01511642
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-10-18 11:17:11 +0100

    fix: reorder script to get around 'docker system prune' issues

--- jx-docker-build.sh
+++ jx-docker-build.sh
@@ -49,30 +49,18 @@ do
 	sed -i.bak -e "s/FROM .*/FROM ${ORG}\/jenkins-base:${TAG}/" Dockerfile.${i}
 	rm Dockerfile.$i.bak
 	head -n 1 Dockerfile.${i}
-	echo "Building ${DOCKER_REGISTRY}/${ORG}/jenkins-${i}:${TAG}"
-    docker build -t ${DOCKER_REGISTRY}/${ORG}/jenkins-${i}:${TAG} -f Dockerfile.${i} .
 done
+
 
 if [ "release" == "${RELEASE}" ]; then
     jx step tag --version $TAG_NUM
 fi
 
-# run the tests against the maven release
-if [ "pr" == "${RELEASE}" ]; then
-	echo "Running test pack..."
-    #jx create post preview job --name owasp --image owasp/zap2docker-stable:latest -c "zap-baseline.py" -c "-t" -c "\$(JX_PREVIEW_URL)" 
-	#docker run --rm \
-    #    -v $PWD/Jenkinsfile-test:/workspace/Jenkinsfile \
-    #    -v /var/run:/var/run \
-    #    -v /etc/resolv.conf:/etc/resolv.conf \
-	#	$ORG/jenkins-maven:$TAG
-	#-e DOCKER_CONFIG=$DOCKER_CONFIG \
-	#-e DOCKER_REGISTRY=$DOCKER_REGISTRY \
-fi
-
 if [ "release" == "${RELEASE}" ]; then
 	for i in "${arr[@]}"
 	do
+		echo "Building ${DOCKER_REGISTRY}/${ORG}/jenkins-${i}:${TAG}"
+    	docker build -t ${DOCKER_REGISTRY}/${ORG}/jenkins-${i}:${TAG} -f Dockerfile.${i} .
    		echo "pushing jenkins-${i} to ${DOCKER_REGISTRY}"
    		docker push ${DOCKER_REGISTRY}/${ORG}/jenkins-${i}:${TAG}
 	done
