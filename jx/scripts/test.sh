82e6d8d4a09e6b4dbdc4b12c54b698f7b4aa47c1
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-12-03 12:19:18 +0000

    feat: enabling steps inside test script

--- jx/scripts/test.sh
+++ jx/scripts/test.sh
@@ -8,11 +8,16 @@ echo "PREVIEW_VERSION=$PREVIEW_VERSION"
 echo "PREVIEW_NAMESPACE=$PREVIEW_NAMESPACE"
 echo "HELM_RELEASE=$HELM_RELEASE"
 
-helm3 --version
+helm3 version
 
-# kubectl create namespace $PREVIEW_NAMESPACE
-# helm3 upgrade --install --namespace $PREVIEW_NAMESPACE
-# check that the Job has completed
-# kubectl delete namespace $PREVIEW_NAMESPACE
+pushd jenkins-x-serverless
+	make build  
+
+	kubectl create namespace $PREVIEW_NAMESPACE
+	helm3 upgrade --install --namespace $PREVIEW_NAMESPACE .
+	# check that the Job has completed
+	kubectl delete namespace $PREVIEW_NAMESPACE
+popd
+
 
 
