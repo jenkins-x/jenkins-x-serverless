bb21d2337f3a595342b5f832fc6e16a7f5f15014
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-01-28 22:34:39 +0000

    chore: further simplfing build to speed up

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,6 +1,6 @@
 FROM JENKINS_BASE_TAG
 
-RUN apt-get install -y build-essential make bzip2 chromedriver chromium pkg-config zip g++ zlib1g-dev unzip python openjdk-8-jdk
+RUN apt-get install -y build-essential make bzip2 chromedriver chromium pkg-config zip g++ zlib1g-dev unzip python
 RUN echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 RUN curl -f https://bazel.build/bazel-release.pub.gpg | apt-key add -
 RUN apt-get update && apt-get install -y bazel
