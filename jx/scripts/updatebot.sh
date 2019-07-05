768b91a4c00d90ad207f1a565d1d54a038e2523d
Pete Muir <pmuir@bleepbleep.org.uk>
2019-07-05 19:14:58 +0100

    chore: switch to the new updatebot

--- jx/scripts/updatebot.sh
+++ jx/scripts/updatebot.sh
@@ -6,5 +6,4 @@ set -o pipefail
 
 echo "VERSION=$VERSION"
 
-updatebot push-regex -r "jenkinsTag:\s(.*)" -v ${VERSION} jx-build-templates/values.yaml
-
+jx step create pr regex --regex "jenkinsTag:\s(.*)" --version ${VERSION} --files jx-build-templates/values.yaml --repo https://github.com/jenkins-x-charts/jx-build-templates.git
